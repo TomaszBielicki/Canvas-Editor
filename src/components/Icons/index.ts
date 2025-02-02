70db70d7b498da5956ecec52229bc2e2066bef7f
Tomasz Bielicki <tomek.bielicki@gmail.com>
2025-02-02 19:37:51 +0100

    imporve exports icons

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as TextIcon } from "./TextIcon";
+export { default as ImageIcon } from "./ImageIcon";
+export { default as BackgroundIcon } from "./BackgroundIcon";
+export { default as WarningIcon } from "./WarningIcon";
