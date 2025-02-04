38f96faa9f493f895c52198e68c1b3ae55bc4f36
Tomasz Bielicki <tomek.bielicki@gmail.com>
2025-02-04 17:17:48 +0100

    responsive buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ export const Button = ({ icon, text, onClick }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className=" bg-[#F7F7F8] flex flex-col justify-evenly items-center w-[365px] h-[256px] rounded-[10px] pb-[12px] pt-[10px]"
+      className=" bg-[#F7F7F8] flex flex-col justify-evenly items-center w-full sm:w-full  xl:w-[365px] h-[256px] rounded-[10px] pb-[12px] pt-[10px]"
     >
       {icon}
       <span>{text}</span>
