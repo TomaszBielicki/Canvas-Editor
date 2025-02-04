9c96929bda140c08f9f17e123850b303c842d938
Tomasz Bielicki <tomek.bielicki@gmail.com>
2025-02-04 11:52:19 +0100

    extract types

---
+++ src/types.ts
@@ -0,0 +1,65 @@
+import { MutableRefObject, ReactNode } from "react";
+
+export interface EditorImage {
+  id: string;
+  src: string;
+  width: number | string;
+  height: number | string;
+  x: number;
+  y: number;
+}
+
+export interface EditorText {
+  id: string;
+  text: string;
+  width: number | string;
+  height: number | string;
+  x: number;
+  y: number;
+}
+
+export interface EditorProps {
+  downloadRef: MutableRefObject<HTMLDivElement | null>;
+  editorBackgroundSrc: string;
+  editorImages: EditorImage[];
+  activeElementId: string | null;
+  setActiveElementId: React.Dispatch<React.SetStateAction<string | null>>;
+  editorText: EditorText[];
+  textColor: string;
+  changeBgInputRef: MutableRefObject<HTMLInputElement | null>;
+  setEditorImages: React.Dispatch<React.SetStateAction<EditorImage[]>>;
+  addImageInputRef: MutableRefObject<HTMLInputElement | null>;
+  setEditorText: React.Dispatch<React.SetStateAction<EditorText[]>>;
+  setEditorBackgroundSrc: React.Dispatch<React.SetStateAction<string>>;
+  setTextColor: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export type ToolbarProps = {
+  addImageInputRef: MutableRefObject<HTMLInputElement | null>;
+  changeBgInputRef: MutableRefObject<HTMLInputElement | null>;
+  setEditorText: React.Dispatch<React.SetStateAction<EditorText[]>>;
+  downloadRef: MutableRefObject<HTMLDivElement | null>;
+  isReset: boolean;
+  setIsReset: React.Dispatch<React.SetStateAction<boolean>>;
+  setEditorBackgroundSrc: React.Dispatch<React.SetStateAction<string>>;
+  setEditorImages: React.Dispatch<React.SetStateAction<EditorImage[]>>;
+  setTextColor: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export type ButtonItem = {
+  icon: ReactNode;
+  text: string;
+  onClick: VoidFunction;
+};
+
+export type ButtonListProps = {
+  setEditorText: React.Dispatch<React.SetStateAction<EditorText[]>>;
+  handleAddImage: () => void;
+  handleChangeBgClick: () => void;
+};
+
+export type ButtonProps = {
+  icon: ReactNode;
+  text: string;
+  onClick: any;
+};
