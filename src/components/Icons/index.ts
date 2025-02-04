9c39a33378956f2f0882e144b7c9122e6b3f9a40
Tomasz Bielicki <tomek.bielicki@gmail.com>
2025-02-04 11:52:10 +0100

    Icons added

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,3 +2,6 @@ export { default as TextIcon } from "./TextIcon";
 export { default as ImageIcon } from "./ImageIcon";
 export { default as BackgroundIcon } from "./BackgroundIcon";
 export { default as WarningIcon } from "./WarningIcon";
+export { default as LogoIcon } from "./LogoIcon";
+export { default as MoveBackIcon } from "./MoveBackIcon";
+export { default as ClickToMoveIcon } from "./ClickToMoveIcon";
